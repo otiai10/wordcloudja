73bf10fe674b1a235fdd6439c6451e229d224ef7
Hiromu OCHIAI <otiai10@gmail.com>
2023-07-14 02:05:10 +0900

    Fix

--- generate.py
+++ generate.py
@@ -11,41 +11,55 @@ from os import path
 from wordcloud import WordCloud
 import MeCab
 
-a = sys.argv[1]
-print(a)
-text = open(a).read()
+def __generate():
+    a = sys.argv[1]
+    print(a)
+    text = open(a).read()
 
-tagger = MeCab.Tagger()
-node = tagger.parseToNode(text)
+    tagger = MeCab.Tagger()
+    node = tagger.parseToNode(text)
 
-# 名詞のみ取り出す
-word_list = []
-while node:
-    word_type = node.feature.split(',')[0]
-    if word_type in ["名詞"]:
-        word_list.append(node.surface)
-    node = node.next
+    # 名詞のみ取り出す
+    word_list = []
+    while node:
+        word_type = node.feature.split(',')[0]
+        if word_type in ["名詞"]:
+            word_list.append(node.surface)
+        node = node.next
 
-print(f'{len(word_list)} words...')
-# 一本のインプットにする
-words = ' '.join(word_list)
-# print(words)
+    print(f'{len(word_list)} words...')
+    # 一本のインプットにする
+    words = ' '.join(word_list)
+    # print(words)
 
-font_path = '/usr/share/fonts/opentype/ipafont-gothic/ipagp.ttf'
-# Generate a word cloud image
-wordcloud = WordCloud(
-    width=1600,
-    height=900,
-    font_path=font_path,
-    background_color='white',
-).generate(words)
+    font_path = '/usr/share/fonts/opentype/ipafont-gothic/ipagp.ttf'
+    stopwords = []
+    try:
+        with open('/data/stopwords.txt') as f:
+            stopwords = f.read().splitlines()
+        print('Stopwords', stopwords)
+    except err:
+        # print(err)
+        print('No stopwords.txt')
 
-# lower max_font_size
-# wordcloud = WordCloud(max_font_size=40).generate(text)
+    # Generate a word cloud image
+    wordcloud = WordCloud(
+        width=1600,
+        height=900,
+        font_path=font_path,
+        background_color='white',
+        stopwords=stopwords,
+    ).generate(words)
 
-# The pil way (if you don't have matplotlib)
-image = wordcloud.to_image()
-output = path.join(a + '.png')
-image.save(output)
+    # lower max_font_size
+    # wordcloud = WordCloud(max_font_size=40).generate(text)
 
-print(output)
+    # The pil way (if you don't have matplotlib)
+    image = wordcloud.to_image()
+    output = path.join(a + '.png')
+    image.save(output)
+
+    print(output)
+
+if __name__ == '__main__':
+    __generate()
